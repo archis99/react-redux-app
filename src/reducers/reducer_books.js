a4b420d23299ac411c74a9816d366925210417d8
SAHOO <archismansahoo@northwesternmutual.com>
2017-09-06 04:21:16 -0500

    redux-flow

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'A Dance With Dragons', pages: 100},
+    {title: 'Behind Closed Doors', pages: 250},
+    {title: 'House By the Lake', pages: 300},
+    {title: 'The ROZABAL Line', pages: 200}
+  ]
+}
